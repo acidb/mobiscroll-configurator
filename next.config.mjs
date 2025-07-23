0055569a26e5e7083687bdaf3294874442241507
Tőtős Armand <armandtotos60@gmail.com>
2025-07-23 12:49:23 +0300

    edit config button

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    domains: ['img.mobiscroll.com'],
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
