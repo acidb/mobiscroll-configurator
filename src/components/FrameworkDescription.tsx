40c8d256430de72b3a830b303fd03dbf34314b83
Tőtős Armand <armandtotos60@gmail.com>
2025-07-25 13:15:10 +0300

    framework description change

--- src/components/FrameworkDescription.tsx
+++ src/components/FrameworkDescription.tsx
@@ -2,9 +2,9 @@
 import React from 'react'
 
 export default function FrameworkDescription() {
-    return <div style={{ maxWidth: '65rem', padding: '0.5rem'}}>
+    return <div style={{ maxWidth: '67rem', padding: '0.5rem'}}>
         <p style={{ marginBottom: '1.5rem' }}>
-            The Frameworks collection defines the available frontend frameworks or languages supported, such as JavaScript, React, Angular, etc. Each framework entry includes a name, a unique slug, optional templates, and icons. The templates provide predefined code snippets or configurations that help users start quickly with a specific framework.
+            The Frameworks collection defines the available frontend frameworks or languages supported, such as JavaScript, React, Angular, etc. Each framework entry includes a name, a unique slug, optional templates, and icons. Templates contain ready-made code snippets that help users get started quickly with the selected framework. These snippets not only serve as examples but also provide the basis for automatically generating components. Based on the templates, the system can configure the necessary settings, data, layouts, and connections tailored to the chosen framework.
         </p>
         <h3 style={{ fontSize: '1.25rem', fontWeight: '600', marginBottom: '0.75rem' }}>What you can do with it:</h3>
         <ul style={{ listStyleType: 'disc', paddingLeft: '1.5rem' }}>
