387679c5ba851c0ff22e86c8131da11f3d5452b5
szily20 <szily20@gmail.com>
2025-07-25 08:59:08 +0300

    fixed build errors

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,23 +1,3 @@
-// import { headers as getHeaders } from 'next/headers'
-// import { getPayload } from 'payload'
-// import React from 'react'
-// import Link from 'next/link'
-// import config from '@/payload.config'
-// import './styles.css'
-// import ConfiguratorClient from './configurator/ConfiguratorClient'
-
-// export default async function HomePage() {
-//   const headers = await getHeaders()
-//   const payloadConfig = await config
-//   const payload = await getPayload({ config: payloadConfig })
-//   const { user } = await payload.auth({ headers })
-
-
-//   return (
-//     <ConfiguratorClient />
-//   )
-// }
-
 import { fetchConfiguratorData } from '@/app/(payload)/fetchData'
 import ConfiguratorClient from './configurator/ConfiguratorClient'
 import { Group, Component, Framework, Preset, Config, GroupedSettings } from './configurator/types'
@@ -29,9 +9,9 @@ import { MbscEventcalendarView } from '@mobiscroll/react'
 export default async function ConfiguratorPage({
   searchParams,
 }: {
-  searchParams: { group?: string; component?: string; framework?: string }
+  searchParams: Promise<{ group?: string; component?: string; framework?: string }>
 }) {
-  const { props } = await fetchConfiguratorData({ query: searchParams })
+  const { props } = await fetchConfiguratorData({ query: await searchParams })
   const headers = await getHeaders()
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
